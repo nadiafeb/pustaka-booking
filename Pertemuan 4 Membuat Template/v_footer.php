23e9f5a163cd2b7e382ed870f6aeccdfc5a2fff4
nadiafeb <95958331+nadiafeb@users.noreply.github.com>
2021-12-13 17:10:27 +0700

    Add files via upload

---
+++ Pertemuan 4 Membuat Template/v_footer.php
@@ -0,0 +1,6 @@
+		<footer>						
+				<a href=>elsa</a>			
+		</footer>
+	</div>
+</body>
+</html>
---
+++ Pertemuan2/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Jimmy
+ Saya tinggal dimana mana hatiku senang
+ Alat musik yang pertama kali saya mainkan yaitu musik box";
+ }
+}
